7655d387d7ce6dd0f6c51bb8926bd0b1e803ecf8
aeroslayys <shwethaparthiban17@gmail.com>
2025-05-04 15:29:05 +0530

    Added PWA

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "feather-player-cache-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
